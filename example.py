a8dd3febbf79a0fb7be0e4fff67f932abbbb1c97
anonymous-doubleblind <84362375+anonymous-doubleblind@users.noreply.github.com>
2023-03-07 17:31:02 +0800

    Add files via upload

---
+++ example.py
@@ -0,0 +1,42 @@
+#import scKTLD1
+#print(scKTLD1.callTLD)
+
+#from scKTLD1 import callTLD
+#print(callTLD)
+
+import numpy as np
+import sys
+import os
+
+import tkinter
+import matplotlib
+import matplotlib.pyplot as plt
+import sklearn.metrics.pairwise as skmetrics
+import seaborn as sns
+
+from scKTLD import edge2adj
+from scKTLD import callTLD
+from scKTLD import displayTLD
+
+matplotlib.use('TkAgg')
+
+hiccolors = ["lightyellow", "red"]
+my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('hicheat', hiccolors)
+matplotlib.cm.register_cmap(cmap = my_cmap)
+featurecolors = ["blue", "white", "red"]
+my_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('featureheat', featurecolors)
+matplotlib.cm.register_cmap(cmap = my_cmap)
+
+path_input = "./data/exp-sc/gm12878_cell7_chr3_sparse.txt"
+graph_edge = np.loadtxt(path_input)
+chr='chr3'
+resolution = 50000
+graph_adj = edge2adj(graph_edge, chr = chr, resolution = resolution, reference = "hg19")
+
+boundary_spec = callTLD(graph_adj)
+
+displayTLD(graph_adj, boundary_spec, 800, 1000, brecon = True)
+
+plt.show()
+#plt.savefig("/media/biology/datadisk/liuerhu/scTAD/biological significance/Tan_50k/commonTADs_show/"+cell+".tiff", dpi = 350)
+plt.close()
